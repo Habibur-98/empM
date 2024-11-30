db2058b3040de5bf7b0d59fe89ff6481eccfa1d7
Habibur-98 <habiburjoynagar786@gmail.com>
2024-11-30 18:14:33 +0530

    its just a test and 1sst commit

---
+++ employeeManagaementAws/src/main/java/com/habib/EmployeeManagaementAwsApplication.java
@@ -0,0 +1,13 @@
+package com.habib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagaementAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagaementAwsApplication.class, args);
+	}
+
+}
